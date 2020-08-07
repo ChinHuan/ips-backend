a0de8045d0c8c383d290196120da42ba4aefb604
Chin Huan <chinhuan99@hotmail.com>
2020-08-08 02:52:15 +0800

    Add new API endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url('^users/$', views.UserView.as_view()),
     url('^users/(?P<userID>.+)/$', views.UserView.as_view()),
     url('^places/$', views.PlaceListView.as_view()),
+    url('^visits/$', views.VisitListView.as_view()),
     url('^visits/(?P<userID>.+)/$', views.VisitListView.as_view()),
 ]

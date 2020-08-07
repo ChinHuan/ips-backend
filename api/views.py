60c3c32fb27f63e4d99d706e957a45fe5652a811
Chin Huan <chinhuan99@hotmail.com>
2020-08-08 02:52:48 +0800

    Use RetrieveAPIView instead of APIView

--- api/views.py
+++ api/views.py
@@ -1,14 +1,18 @@
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
-from .serializers import UserSerializer, PlaceSerializer, VisitSerializer
+from rest_framework.mixins import UpdateModelMixin
+from rest_framework import status
+from django.utils.dateparse import parse_duration
+
+from .serializers import UserSerializer, PlaceSerializer, VisitPlaceSerializer, VisitSerializer
 from .models import User, Place, Visit
 
 class PlaceListView(ListAPIView):
     serializer_class = PlaceSerializer
     queryset = Place.objects.all()
 
-class UserView(APIView):
+class UserView(RetrieveAPIView):
     def get(self, request, userID):
         user = User.objects.get(userID=userID)
         return Response(UserSerializer(user).data)

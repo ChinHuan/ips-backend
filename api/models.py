30cb84b59f23eaf863d72d94a3427bacc18ffab4
Chin Huan <chinhuan99@hotmail.com>
2020-08-22 10:10:51 +0800

    Add PhotoUrl to user model

--- api/models.py
+++ api/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
     userEmail = models.CharField(max_length=60)
     userContact = models.CharField(max_length=20, null=True, blank=True)
     userRole = models.IntegerField(choices=Role.choices)
+    userPhotoUrl = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return '{}: {}'.format(self.userID, self.userName)

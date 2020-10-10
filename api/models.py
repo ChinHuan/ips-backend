642fccea936f87ec5c0409dbae0d21e936163a61
Chin Huan <chinhuan99@hotmail.com>
2020-10-10 22:43:49 +0800

    Increase maximum tagID length

--- api/models.py
+++ api/models.py
@@ -27,7 +27,7 @@ class Visit(models.Model):
     visitID = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    tagID = models.CharField(max_length=10)
+    tagID = models.CharField(max_length=20)
     datetime = models.DateTimeField(auto_now_add=True)
     duration = models.DurationField(null=True, blank=True)
 
@@ -36,7 +36,7 @@ class Visit(models.Model):
 
 class Coordinate(models.Model):
     coordinateID = models.AutoField(primary_key=True)
-    tagID = models.CharField(max_length=10)
+    tagID = models.CharField(max_length=20)
     datetime = models.DateTimeField(auto_now_add=True)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     x = models.FloatField()
